ed26927ddf9ae08a7d6afcd153d53662abae7e38
Bouclette-B <anne.boucle@lilo.org>
2020-07-08 08:18:55 +0200

    tentative js pour faire apparaitre-disparitre la description de  la vidéo

--- _site/assets/js/themes-utils.js
+++ _site/assets/js/themes-utils.js
@@ -9,3 +9,23 @@ function switchTheme(themeName = "dark"){
 
 var myTheme = localStorage.getItem(STORAGE_KEY);
 switchTheme(myTheme);
+
+//Fonction boutons plus/moins description des vidéos ///
+var plus = document.getElementById("plus");
+var moins = document.getElementById("moins");
+var d1 = document.getElementById("d1");
+var d2 = document.getElementById("d2");
+
+plus.addEventListener("click", () => {
+    if(getComputedStyle(d1).display != "none") {
+        d1.style.display = "none" ;
+        d2.style.display = "block" ;
+    } 
+})
+
+moins.addEventListener("click", () => {
+    if(getComputedStyle(d2).display != "none") {
+        d2.style.display = "none" ;
+        d1.style.display = "block" ;
+    } 
+})
